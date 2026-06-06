060e5bcd709ebafe2d21622a487403c6336374aa
Muhammad Keanu <m.keanusulthansyah@gmail.com>
2026-06-06 12:54:40 +0700

    bismillah

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,16 @@ import React from 'react'
 const App = () => {
   return (
     <div className='w-screen h-screen'>
-      <a-scene mindar-image="imageTargetSrc:src\assets\targets.mind" color-space="sRGB" renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
-      <a-assets>
-        <a-asset-item id="avatarModel" crossOrigin="anonymous" src="src\assets\model.gltf"></a-asset-item>
-      </a-assets>
+<a-scene mindar-image="imageTargetSrc: src\assets\targets.mind; uiError:no; uiLoading:no; filterMinCF:0.001; filterBeta: 0.001;" color-space="sRGB" renderer="colorManagement: true, physicallyCorrectLights: false" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: true">
+        <a-assets>
+            <a-asset-item id="avatarModel" src="src\assets\model.gltf"></a-asset-item>
+        </a-assets>
 
-      <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
-      <a-entity mindar-image-target="targetIndex: 0">
-        <a-plane src="#card" position="0 0 0" height="0.552" width="1" rotation="0 0 0"></a-plane>
-        <a-gltf-model rotation="90 0 0 " position="0 0 0" scale="1 1 1"></a-gltf-model>
-      </a-entity>
-    </a-scene>
+        <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
+        <a-entity mindar-image-target="targetIndex: 0">
+            <a-gltf-model rotation="90 0 0" position="0 0 -1.5" scale="1 1 1" src="#avatarModel"></a-gltf-model>
+        </a-entity>
+        </a-scene>
     </div>
   )
 }
