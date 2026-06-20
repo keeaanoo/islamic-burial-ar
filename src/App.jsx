1e9e2e8b83a0929ba93d61e2e65a344b6ee1e645
Muhammad Keanu <m.keanusulthansyah@gmail.com>
2026-06-20 15:59:39 +0700

    3d

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import 'aframe-extras'
 import targetsMind from './assets/targets.mind?url'
-import step1 from './assets/step1.gltf?url'
 import animated from './assets/animated.gltf?url'
+import gltf from './assets/model-backup-textured.gltf?url'
 
 const App = () => {
   return (
     <div className='w-screen h-screen'>
       <a-scene mindar-image={`imageTargetSrc: ${targetsMind}; uiError:no; uiLoading:no; filterMinCF:0.001; filterBeta: 0.001;`} color-space="sRGB" renderer="colorManagement: true, physicallyCorrectLights: true" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: true">
         <a-assets>
-          <a-asset-item id="avatarModel" src={animated}></a-asset-item>
+          <a-asset-item id="avatarModel" src={gltf}></a-asset-item>
         </a-assets>
 
         <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
         <a-entity mindar-image-target="targetIndex: 0">
-            <a-gltf-model rotation="90 0 0" position="0 0 -0.5" scale="0.4 0.4 0.4" src="#avatarModel" animation-mixer="clip: step1"></a-gltf-model>
+            <a-gltf-model rotation="90 0 0" position="0 0 -0.6" scale="0.4 0.4 0.4" src="#avatarModel" animation-mixer="clip: step4"></a-gltf-model>
         </a-entity>
         </a-scene>
     </div>
